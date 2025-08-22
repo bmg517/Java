f5377d8e962b3f6eebe28d91006288fd85a4890d
Bharat Mohan Gupta <bharatgupta87567+github@gmail.com>
2025-08-22 14:24:03 +0530

    Added

---
+++ Java3_Userinput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Java3_Userinput {
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter Your Name");
+        // String name = sc.nextLine();
+        // System.out.println("Welcome "+ name + " To Coding");
+
+
+        // Create A Program To Add Two Numbers  
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The First Number");
+        int num1 = sc.nextInt();
+        System.out.println("Enter The Second Number");
+        int num2 = sc.nextInt();
+        System.out.println("Sum Of Two Number Is: ");
+        int sum = num1 + num2;
+        System.out.println(sum);
+
+
+
+    }
+}
---
+++ Java4_TypeConversion.java
@@ -0,0 +1,7 @@
+public class Java4_TypeConversion {
+
+    public static void main(String[] args) {
+        int value = (int) 344.4d;
+        System.out.println(value);
+    }
+}
