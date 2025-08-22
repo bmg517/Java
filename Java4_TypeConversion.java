273516500cb998c1836ae7e66ccbb96618c05379
Bharat Mohan Gupta <bharatgupta87567+github@gmail.com>
2025-08-23 01:03:17 +0530

    Added

--- Java4_TypeConversion.java
+++ Java4_TypeConversion.java
@@ -2,6 +2,10 @@ public class Java4_TypeConversion {
 
     public static void main(String[] args) {
         int value = (int) 344.4d;
+        // Data will Loss While Converting From Double To Integer
         System.out.println(value);
+
+        double money = (double)33.0f;
+        System.out.println(money);
     }
 }
---
+++ Java5_Operators.java
@@ -0,0 +1,38 @@
+public class Java5_Operators {
+
+    public static void main(String[] args) {
+        // 1. Assingement Operators
+
+    //    int num1 = 19;
+    //    System.out.println(num1);
+    //    int num2 = num1;
+    //    System.out.println(num2);
+
+       // Write A Program To Swap Two Number
+       
+    //    int a =10;
+    //    int b= 20;
+    //    int temp = a;
+    //    a = b;
+    //    b=temp;
+    //    System.out.println(a);
+    //    System.out.println(b);
+
+       // 2. Arithmetic Operators
+
+
+       int x = 10;
+       int y = 3;
+       System.out.println(x+y);
+       System.out.println(x-y);
+       System.out.println(x*y);
+       System.out.println(x/y);
+
+    //    Modulas Operators
+       System.out.println(x%y);
+       
+
+
+       
+    }
+}
