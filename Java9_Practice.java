3b024bfe0f99357c2fb12e2d10dee3be9e4a796e
Bharat Mohan Gupta <bharatgupta87567+github@gmail.com>
2025-08-23 16:56:16 +0530

    Added

---
+++ Java9_Practice.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+public class Java9_Practice {
+
+    public static void main(String[] args) {
+        // Q1. Create A Program That Take Two Number And Show All Arithmetic Operators.
+        // Scanner add = new Scanner(System.in);
+        // System.out.println("Enter The First Number");
+        // int num = add.nextInt();
+        // System.out.println("Enter The Second Number");
+        // int num1 = add.nextInt();
+        // System.out.println("The Addition Of Two Number is : ");
+        // int sum = num+num1;
+        // System.out.println(sum);
+        // System.out.println("The Substraction Of Two Value Is : ");
+        // int sub = num-num1;
+        // System.out.println(sub);
+        // System.out.println("The Multiplication Of Two Number Is : ");
+        // int multi = num*num1;
+        // System.out.println(multi);
+        // System.out.println("The Division Of Two Number Is : ");
+        // int divi = num/num1;
+        // System.out.println(divi);
+        // System.out.println("The Modulas Of Two Number Is : ");
+        // int mod = num%num1;
+        // System.out.println(mod);
+
+
+        // Q2. Create A Program To Calculate The Two Floating Numbers.
+
+    //    Scanner value = new Scanner(System.in);
+    //    System.out.println("Enter The First Number : "); 
+    //    float num = value.nextFloat();
+    //    System.out.println("Enter The Second Number : ");
+    //    float num1 = value.nextFloat();
+    //    System.out.println("The Sum Of Two Value Is : ");
+    //    float sum = num*num1;
+    //    System.out.println(sum);
+
+       
+        // Q3. Create A Program To Calculate The Perimeter Of A Rectangle.
+        //     Perimeter Of Rectangle Is ABCD = A+B+C+D.
+
+        // Scanner Perimeter = new Scanner(System.in);
+        // System.out.println("Enter The Value Of Side A : ");
+        // int sum1 = Perimeter.nextInt();
+    
+        // System.out.println("Enter The Value Of Side B : ");
+        // int sum2 = Perimeter.nextInt();
+
+        // System.out.println("Enter The Value Of Side C : ");
+        // int sum3 = Perimeter.nextInt();
+
+
+        // System.out.println("Enter The Value Of Side D : ");
+        // int sum4 = Perimeter.nextInt();
+
+        // int sum = sum1+sum2+sum3+sum4;
+        // System.out.println("The Perimeter Of Side ABCD Is : ");
+        // System.out.println(sum);
+
+
+        // Q4. Create A Program To Calculate The Area Of Triangle.
+        //     Area Of Triangle = 1/2*B*H.
+
+        // Scanner area = new Scanner(System.in);
+        // System.out.println("Enter The Value Of Breath : ");
+        // int breath = area.nextInt();
+        // System.out.println("Enter The Value of Height : ");
+        // int height = area.nextInt();
+        // System.out.println("The Area OF Triangle Is : ");
+        // int area1 = (breath*height)*1/2;
+        // System.out.println(area1);
+
+        // Q5. Create A Program To Calculate Simple Interest.
+        //     Simple Interest =(PxTxR)x100.
+
+        
+        // Q6. Create A Program To Calculate A Compound Interest.
+        //     Compund Interst =P(1+R/100)t.
+        // Q7. Create A Program To Convert Fehrenheit To Celsius.
+        //     C = (F-32)x5/9.   
+    }
+}
