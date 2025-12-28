097bded6a1de055506c070a808d5b0a0dd49c44d
Bharat Mohan Gupta <bharatgupta87567+github@gmail.com>
2025-12-28 23:01:21 +0530

    Added

---
+++ operator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class operator {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter The Value Of A: ");
+    int a = input.nextInt();
+    System.out.println("Enter The Value Of B :");
+    int b = input.nextInt();
+    int temp = a;
+    a=b;
+    b = temp;
+    System.out.println("After Swapping");
+    System.out.println("The Value Of A :" + a);
+    System.out.println("The Value Of B ;" + b);
+  
+
+
+
+  }
+}
--- userinput.java
+++ userinput.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class userinput {
   public static void main(String[] args) {
     System.out.println("User Input In Java");
-    System.out.println(" ");
+    System.out.println("  ");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter The First Name");
+    String fname = input.next();
+    System.out.println("Enter The Last Name");
+    String lname = input.next();
+    System.out.print("The Full Name Is: ");
+    String name = fname + " " + lname;
+    System.out.println(name);
+
+
 
     
   }
