4022eae5fb5658bdc64caf666d4561894ce10e04
Bharat Mohan Gupta <bharatgupta87567+github@gmail.com>
2026-01-07 22:25:52 +0530

    Add

---
+++ Exercise/Exercises.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Exercises {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+                                  // Practice No 1
+    // /* 
+    System.out.println("Please Enter Your Name: ");
+    String name = input.nextLine();
+    System.out.println("Welcome " + name);
+    // */
+
+                                  //  Practice No 2
+    /*                               
+    System.out.println("Enter The First Number: ");
+    int num1 = input.nextInt();
+    System.out.println("Enter The Second Number: ");
+    int num2 = input.nextInt();
+    System.out.println("Sum Of The Two Number is: ");
+    int sum = num1 + num2;
+    System.out.println(sum);
+    */
+
+                                  // Practice No 8
+
+    /*
+    System.out.println("Result Of + - * /");
+    int a = 20;
+    int b = 10;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a+b);
+    System.out.println(a%b);
+    */
+
+
+                                 // Practice No 9
+
+    /* 
+    System.out.println("Product of Two Floating Value");
+    float f1 = 10.2f;
+    float f2 = 11f;
+    float sum = f1+f2;
+    System.out.println(sum);
+     */
+
+                                // Question No 10
+
+    /*
+    System.out.println("Perimeter Of A Triangle");
+    int sidea = 11;
+    int sideb = 12;
+    int sidec = 13;
+    int sided = 14;
+    int sum = sidea+sideb+sidec+sided;
+    System.out.println("The Sum Of The Side A,B,C,D is: "+ sum);
+    */
+
+                                // Question 11
+    /*  
+    System.out.println("Calculation Of Area Of Triangle");
+    int l = 10;
+    int b = 5;
+    int area = l*b/2;
+    System.out.println("Area Of Triangle is : " + area);
+    */
+
+
+  }
+}
